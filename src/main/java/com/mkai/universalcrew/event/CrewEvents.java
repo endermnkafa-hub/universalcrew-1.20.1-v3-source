618194dba4ae0df1f627e97e67ae7b570e12fc26
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-27 11:23:28 +0300

    Auto update 2026-08-27 11:23:28

--- src/main/java/com/mkai/universalcrew/event/CrewEvents.java
+++ src/main/java/com/mkai/universalcrew/event/CrewEvents.java
@@ -2862,6 +2862,33 @@ public static void healRecruit(
             captain
     );
 }
+       private static int countItem(
+        Player player,
+        Item item
+) {
+
+    int total = 0;
+
+    for (
+            int i = 0;
+            i < player.getInventory()
+                    .getContainerSize();
+            i++
+    ) {
+
+        ItemStack stack =
+                player.getInventory()
+                        .getItem(i);
+
+        if (stack.is(item)) {
+
+            total +=
+                    stack.getCount();
+        }
+    }
+
+    return total;
+}
     // =========================================================
     // RECRUIT TICK
     // =========================================================
